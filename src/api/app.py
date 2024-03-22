145a0de2a6d6500cfcd0fa1a50d1ef1385e92752
Fernando de la Calle Silos <fernando.delacalle@six-group.com>
2024-03-22 18:20:02 +0100

    push api image

---
+++ src/api/app.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/suma")
+def suma(a: int, b: int):
+    return a + b
+
+
+@app.get("/resta")
+def resta(
+    a: int,
+    b: int,
+):
+    return a - b
